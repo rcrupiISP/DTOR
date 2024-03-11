631e532f1c747159d941e0cb05e98451dabaac5c
rcrupiISP <92302358+rcrupiISP@users.noreply.github.com>
2024-03-11 10:46:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Intesa SanPaolo S.p.A
+# Copyright 2024 Intesa Sanpaolo S.p.A
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
